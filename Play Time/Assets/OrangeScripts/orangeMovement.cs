67bf5ae0595202e02742656672c3606be91d19cd
cagomez6 <64633124+cagomez6@users.noreply.github.com>
2021-10-16 16:09:18 -0700

    changed Script name to orangeMovement

--- Play Time/Assets/OrangeScripts/orangeMovement.cs
+++ Play Time/Assets/OrangeScripts/orangeMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class orangeMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float walkSpeed;
