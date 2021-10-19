6bc56cbfa3b50ad989a9e77ff85c996e8f9c0f57
nghamade <nghamade@gmail.com>
2021-10-19 10:58:32 -0700

    Ground Check Changes
    Switched from Raycast to Boxcast so the jumping check is more accurate

--- Play Time/Assets/Scripts/PlayerMovement.cs
+++ Play Time/Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,41 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float moveSpeed = 10;
     [SerializeField] private float walkSpeed = 5;
-    [SerializeField] private float runSpeed = 20;
+    //[SerializeField] private float runSpeed = 20;
     [SerializeField] private float airSpeed = 10;
 
     private Vector3 moveDirection;
     private Vector3 velocity;
     private RaycastHit Hit;
 
+    [Header("Ground Elements")]
     [SerializeField] private bool isGrounded;
     [SerializeField] private float groundCheckDistance = 1.25f;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float gravity = 9.81f;
 
+    [Header("Jump")]
     [SerializeField] private float jumpHeight;
 
+    [Header("Character Components")]
     private CharacterController controller;
+    private BoxCollider playerCollider; 
+    private Rigidbody playerRB;
+
     //private Animator anim;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        playerCollider = GetComponent<BoxCollider>(); 
+        playerRB = GetComponent<Rigidbody>(); 
+        
         //anim = GetComponentInChildren<Animator>();
     }
 
@@ -34,46 +44,38 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+
+    //IsGrounded function
+    private bool IsGrounded()
+    {
+        Debug.Log("Checking if Grounded"); 
+        //constructing box cast which needs (center point, size, rotation, direction, max distance to project cast, layerMask)
+            //LAYER MASK -> what layers the raycast will hit
+            //down because we are detecting the floor and is just slightly offset from player position
+        bool hit = Physics.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, Vector3.down, Quaternion.identity, groundCheckDistance, groundMask);
+        
+        //The box cast will return False if the character is on the ground, so it returns the opposite of that so it isGrounded is true when character on ground
+        return !hit; 
+    }
+    
     private void Move()
     {
         //make sure to put any floors on the "ground" layer
-        //isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance,groundMask);
-        Debug.DrawRay(transform.position, new Vector3(0, -groundCheckDistance, 0), Color.green);
-        Ray groundcast = new Ray(transform.position, -Vector3.up);
-        if (Physics.Raycast(groundcast, out Hit, groundCheckDistance))
-        {
-            if (Hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
-            {
-                isGrounded = true;
-            }
-            else
-            {
-                isGrounded = false;
-            }
-        }
-        else
-        {
-            isGrounded = false;
-        }
-
-
-        if (isGrounded && velocity.y < 0)
+        
+        if (IsGrounded() && velocity.y < 0)
         {
             velocity.y = -2f;
         }
         float moveZ = Input.GetAxis("Vertical") * moveSpeed;
         float moveX = Input.GetAxis("Horizontal") * moveSpeed;
-        if (isGrounded)
+        
+        if (IsGrounded())
         {
             print("grounded");
             //anim.SetBool("grounded", true);
             if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
             {
                 Walk();
-            }
-            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
-            {
-                Run();
             }
             else if (moveDirection == Vector3.zero)
             {
@@ -110,17 +112,18 @@ public class PlayerMovement : MonoBehaviour
         //anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
     }
 
+    
     private void Walk()
     {
         moveSpeed = walkSpeed;
         //anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
     }
 
-    private void Run()
+/*    private void Run()
     {
         moveSpeed = runSpeed;
         //anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
-    }
+    }*/
 
     private void Jump()
     {
