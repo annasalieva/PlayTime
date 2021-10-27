f2c293361dc9ec96fda7fd594c85b49f6833afbd
Jaydeveloper394 <jaydevp394@gmail.com>
2021-10-26 20:00:25 -0700

    Added swap functionality in PauseMenuTest (press Q or E to swap between Orange and Lemon). Also added Cinemachine and A* Pathfinding to be used for character-following.

---
+++ Play Time/Assets/Scripts/CharacterSwap.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CharacterSwap : MonoBehaviour
+{
+    public Transform character;
+    public List<Transform> characterList;
+    public int characterIndex;
+
+    public CinemachineVirtualCamera cam;
+    public ParticleSystem particles;
+
+    void Start()
+    {
+        if(character == null && characterList.Count >= 1)
+        {
+            character = characterList[0];
+        }
+        SwapCharacters();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if(characterIndex == 0)
+            {
+                characterIndex = characterList.Count - 1;
+            }
+            else
+            {
+                characterIndex -= 1;
+            }
+            SwapCharacters();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (characterIndex == characterList.Count - 1)
+            {
+                characterIndex = 0;
+            }
+            else
+            {
+                characterIndex += 1;
+            }
+            SwapCharacters();
+        }
+    }
+
+    public void SwapCharacters()
+    {
+        character = characterList[characterIndex];
+        character.GetComponent<CharacterController>().enabled = true;
+
+        particles.transform.position = character.position;
+        particles.Play();
+
+        //disable movement for all other characters
+        for(int i = 0; i < characterList.Count; i++)
+        {
+            if(characterList[i] != character)
+            {
+                characterList[i].GetComponent<CharacterController>().enabled = false;
+            }
+        }
+        cam.LookAt = character;
+        cam.Follow = character;
+    }
+}
