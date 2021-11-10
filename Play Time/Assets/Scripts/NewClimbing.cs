70d127668ffcd18ae404b314ef3ab753066dde45
nghamade <nghamade@gmail.com>
2021-11-09 20:56:25 -0800

    In Progress Climbing
    Sending the in progress climbing script

--- Play Time/Assets/Scripts/NewClimbing.cs
+++ Play Time/Assets/Scripts/NewClimbing.cs
@@ -4,6 +4,91 @@ using UnityEngine;
 
 public class NewClimbing : MonoBehaviour
 {
+    
+    [Header("Colliders")]
+    [SerializeField]
+    private Collider LADDER_COLLIDER; //main collider for the ladder
+    
+    [SerializeField]
+    private Collider ENTRANCE_COLLIDER;
+    
+    [SerializeField]
+    private Collider EXIT_COLLIDER;
+
+    [Header("Movement")]
+    [SerializeField]
+    private float ClimbingSpeed = 1f; //how fast the character moves while climbing
+    
+    [Header("Climbing Bounds")]
+    
+    [SerializeField]
+    private GameObject Lemon; //Lemon's transform
+    private float X_MIN; 
+    private float X_MAX; 
+    private float Y_MIN; 
+    private float Y_MAX; 
+
+    [Header("Bools")]
+    private bool isClimbing = false; 
+
+    void Start()
+    {
+        //gets x min/max of collider
+        X_MIN = LADDER_COLLIDER.bounds.min.x; 
+        X_MAX = LADDER_COLLIDER.bounds.max.x; 
+
+        //gets y min/max of collider
+        Y_MIN = LADDER_COLLIDER.bounds.min.y; 
+        Y_MAX = LADDER_COLLIDER.bounds.max.y; 
+    }
+    
+    void Climb()
+    {
+        //climbing scripts go here
+        //restrict lemon on the x min/max and y min/max
+
+        if (Input.GetKeyDown(KeyCode.E)) //stop climbing once the player hits E again
+        {
+            isClimbing = false; //player is no longer climbing
+        }
+        else 
+        {
+            //allow player to move around at specified climbing speed
+            //restrict them if their x and y are lower/higher than min/max
+            
+        }
+    
+    
+    
+    }
+    void OnTriggerStay(Collider box) //is called as long as the player is in the collider
+    {
+        if (box == ENTRANCE_COLLIDER)
+        {
+            //if the player has entered the entrance collider -> we want to do climbing stuff once player hits e
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //player hits E and now we want to start climbing
+                Climb();
+                //player is now in climbing state
+                isClimbing = true; 
+            }
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider box)
+    {
+        if (box == EXIT_COLLIDER)
+        {
+            //player has made it to the end and is no longer climbing
+            isClimbing = false; 
+
+            //call climbing exit animation here
+        }
+    }
+    
+    
     /*
     LOGIC:
 
