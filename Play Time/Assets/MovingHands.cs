8f9c067a5af5e4756584cb33cd0aee4b99e23a9f
Tyler Pasela <paselat99@gmail.com>
2021-11-30 22:26:13 -0800

    Updated hand script

--- Play Time/Assets/MovingHands.cs
+++ Play Time/Assets/MovingHands.cs
@@ -5,20 +5,42 @@ using UnityEngine;
 public class MovingHands : MonoBehaviour
 {
     public float steps;
-    public float time;
     private Transform pos1;
     private Transform pos2;
     private Transform targetPos;
+    public bool buttonon = false;
     // Start is called before the first frame update
     void Start()
     {
-        pos1 = this.transform;
-        pos2 = this.transform;
-        pos2.position+= new Vector3(0,0,9);
+        pos1 = transform;
+        pos2 = transform;
+        pos2.position+= new Vector3(0,0,-9);
+        targetPos = pos2;
     }
     // Update is called once per frame
     void Update()
     {
+        if (buttonon){
+            transform.position = Vector3.MoveTowards(transform.position, targetPos.position, steps*Time.deltaTime);
+            if (Vector3.Distance(transform.position, targetPos.position) < 0.001f)
+            {
+                if (targetPos == pos2)
+                {
+                    wait();
+                    targetPos = pos1;
+                }
+                else
+                {
+                    wait();
+                    targetPos = pos2;
+                }
+            }
+        }
         
     }
+
+    IEnumerator wait(){
+        yield return new WaitForSeconds(5);
+    }
+
 }
